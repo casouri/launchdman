86aa8c3957ed9df7979c12803f142e606fd8c7bb
Yuan Fu <casouri@gmail.com>
2017-10-18 01:30:30 -0400

    Ready to upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='launchdman',
+    version='0.1',
+    description='The launchd parser and manager for Python',
+    url='https://github.com/casouri/launchdman',
+    author='Yuan Fu',
+    author_email='yuan.k.fu@gmail.com',
+    license='MIT',
+    packages=['launchdman'],
+    zip_safe=False)
